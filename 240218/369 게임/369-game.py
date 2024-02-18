81404b17cd1cde16d4924bd3088a61eafb8f0e63
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-02-18 15:05:18 +0900

    Title: 369 게임, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240218/369 게임/369-game.py
+++ 240218/369 게임/369-game.py
@@ -10,13 +10,12 @@ for i in range(1, n+1):
     elif (i % 10 == 3 or i % 10 == 6 or i % 10 == 9):
         print(0, end=" ")
     
+    elif (i // 10 == 3 or i // 10 == 6 or i // 10 == 9):
+        print(0, end=" ")
+        
     else:
         print(i, end=" ")
     
 
 
-
-
-
-        # 20
-        # 31 ~ 35
+# 31, 32 ,34, 35
