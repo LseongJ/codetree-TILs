3af1be3cb09f2d7d4e8f017521a739bffa6ef05a
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-02-18 19:36:33 +0900

    Title: 소수 판별기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240218/소수 판별기/prime-number-discriminator.py
+++ 240218/소수 판별기/prime-number-discriminator.py
@@ -4,8 +4,11 @@ ans = False
 for i in range(2, n):
     if (n % i == 0):
         ans = True
-        print("C")
         break
     else:
-        print("P")
         break
+
+if ans == True:
+    print("C")
+else:
+    print("P")
