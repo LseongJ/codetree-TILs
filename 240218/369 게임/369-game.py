cfbc924d836882582ee7e394ebc5edf3848cdbb2
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-02-18 15:03:05 +0900

    Title: 369 게임, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240218/369 게임/369-game.py
+++ 240218/369 게임/369-game.py
@@ -1,30 +1,22 @@
 n = int(input())
 
 for i in range(1, n+1):
-
     if (i == 1 or i == 2):
         print(i, end=" ")
-
-    elif (3 <= i <= 10):
-        if (i % 3 == 0):
-            print(0, end=' ')
-        else:
-            print(i, end=" ")
     
-    elif (i > 10):
-        if (i % 3 == 0):
-            print(0, end=" ")
-        elif ((i % 10) % 3 == 0):
-            print(0, end=" ")
-        else:
-            print(i, end=" ")
+    elif (i % 3 == 0):
+        print(0, end=" ")
+    
+    elif (i % 10 == 3 or i % 10 == 6 or i % 10 == 9):
+        print(0, end=" ")
+    
+    else:
+        print(i, end="")
+    
 
 
 
 
 
-        # 13 % 3 = 1
-        # 23 % 3 = 2
-        # 33 % 3 = 0
-
-        # 31 // 10 = 3
+        # 20
+        # 31 ~ 35
