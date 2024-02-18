0db5df52854a3198e22d6d0125f2d03bd1d7fb90
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-02-18 14:57:28 +0900

    Title: 369 게임, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240218/369 게임/369-game.py
+++ 240218/369 게임/369-game.py
@@ -16,7 +16,8 @@ for i in range(1, n+1):
             print(0, end=" ")
         elif ((i % 10) % 3 == 0):
             print(0, end=" ")
-            
+        else:
+            print(i, end=" ")
 
 
 
