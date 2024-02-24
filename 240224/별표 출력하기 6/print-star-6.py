144fbfdfad88ee99e28f5488cb9491ffd6e89b65
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-02-24 16:03:44 +0900

    Title: 별표 출력하기 6, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240224/별표 출력하기 6/print-star-6.py
+++ 240224/별표 출력하기 6/print-star-6.py
@@ -1,3 +1,15 @@
-n = 2
+n = 4
 
-for i
+for i in range(n, 0, -1):
+    for k in range(n - i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+
+for i in range(1, n):
+    for j in range(n - i - 1):   # 공백 : 2 -> 1 -> 0
+        print(" ", end=" ")
+    for k in range(2*i + 1):
+        print("*", end=" ")
+    print()
