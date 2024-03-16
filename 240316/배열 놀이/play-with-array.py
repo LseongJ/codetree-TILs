be239955ec31f99bae424aa4b8e6a16f1e8f5ba9
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-03-16 09:31:01 +0900

    Title: 배열 놀이, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240316/배열 놀이/play-with-array.py
+++ 240316/배열 놀이/play-with-array.py
@@ -1,16 +1,18 @@
 n, q = map(int, input().split())
 n_elem = list(map(int, input().split()))
 
-# q개의 질의응답
-for _ in range(q):
-  question =  list(map(int, input().split()))
-  if question[0] == 1:
-    print(n_elem[question[1] - 1])
-  elif question[0] == 2:
-    if question[1] in n_elem:
-      print(n_elem.index(question[1]) + 1)
+for i in range(q): # 질의 q회 반복
+  quest = list(map(int, input().split()))
+  
+  if quest[0] == 1:
+    print(n_elem[quest[1] - 1])   # 1 a > a번째 원소를 출력
+
+  elif quest[0] == 2:
+    if quest[1] in n_elem:
+      print(n_elem.index(quest[1]) + 1)
     else:
       print(0)
-  elif question[0] == 3:
-    for i in n_elem[question[1]-1:question[2]:1]:
-      print(i, end=' ')
+
+  elif quest[0] == 3:
+    for k in n_elem[quest[1] - 1 : quest[2]]:
+      print(k, end = " ")
