aecd33ad35fc399138aa559ad81d8257b107eca8
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-03-16 19:04:47 +0900

    Title: 배열의 평균, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/배열의 평균/ave-of-array.py
+++ 240316/배열의 평균/ave-of-array.py
@@ -8,7 +8,7 @@ sum_row = 0
 for i in range(2):
   for j in range(len(arr[i])):
     sum_row += arr[i][j]
-  print(sum_row / 4, end=" ")
+  print(f"{sum_row / 4:.1f}", end=" ")
   sum_row = 0
 print()
 
@@ -17,11 +17,12 @@ sum_col = 0
 for i in range(4):
   for j in range(2):
     sum_col += arr[j][i]
-  print(sum_col / 2, end=" ")
+  print(f"{sum_col / 2:.1f}", end=" ")
   sum_col = 0
 print()
 
 # 전체 평균
 sum1 = sum(arr[0])
 sum2 = sum(arr[1])
-print((sum1 + sum2) / 8)
+ans = (sum1 + sum2) / 8
+print(f"{ans:.1f}")
