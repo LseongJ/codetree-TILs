2d3b0a40becc853e7cb3793b243c37d477db97d0
LseongJ <115673103+LseongJ@users.noreply.github.com>
2024-04-02 14:50:29 +0900

    Title: 부분문자열 위치 구하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240402/부분문자열 위치 구하기/find-location-of-substring.py
@@ -0,0 +1,14 @@
+word = input()
+obj = input()
+
+w = len(word)
+o = len(obj)
+
+exist = False
+
+for i in range(w - o + 1):
+    # 문자열 obj와 word[i:i+o]가 같은지 확인
+    if word[i:i+o] == obj:
+        exist = True
+        print(i)  # 첫 번째로 등장하는 위치를 출력
+        break
